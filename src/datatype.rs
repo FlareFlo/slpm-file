9ded655832c16333bfe5b6d2247379415d8a8ff7
flareflo <florianlipp17@gmail.com>
2021-10-13 11:13:32 +0200

    Impl toString for datatype.rs
    Took 2 minutes

--- src/datatype.rs
+++ src/datatype.rs
@@ -3,3 +3,12 @@ pub enum DataType {
 	Password = 0,
 	File = 1,
 }
+
+impl ToString for DataType {
+	fn to_string(&self) -> String {
+		match self {
+			self::DataType::Password => {"Password".to_owned()},
+			self::DataType::File => {"File".to_owned()}
+		}
+	}
+}
